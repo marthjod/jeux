542837b6918665b867dfb52503144973a024912a
marthjod@gmail.com <marthjod@gmail.com>
2013-12-18 21:57:00 +0100

    update game PEs based on DTOs

--- JeuxEJB/ejbModule/de/fhb/jeux/session/UpdateGameBean.java
+++ JeuxEJB/ejbModule/de/fhb/jeux/session/UpdateGameBean.java
@@ -1,6 +1,8 @@
 package de.fhb.jeux.session;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.ListIterator;
 
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
@@ -11,6 +13,7 @@ import de.fhb.jeux.dao.GameDAO;
 import de.fhb.jeux.dto.GameDTO;
 import de.fhb.jeux.dto.GameSetDTO;
 import de.fhb.jeux.model.IGame;
+import de.fhb.jeux.model.IGameSet;
 import de.fhb.jeux.persistence.ShowdownGameSet;
 
 @Stateless
@@ -31,7 +34,7 @@ public class UpdateGameBean implements UpdateGameRemote, UpdateGameLocal {
 	public boolean updateGame(GameDTO gameDTO) {
 		boolean success = false;
 		boolean updated = false;
-		int i;
+		int numDTOs = 0;
 
 		IGame game = gameDAO.getGameById(gameDTO.getId());
 		if (game != null && gameDTO != null) {
@@ -45,47 +48,146 @@ public class UpdateGameBean implements UpdateGameRemote, UpdateGameLocal {
 			if (existingSets.size() == newSets.size()) {
 
 				// copy scores from DTO to Entity
+				// and try to determine set winner
+
+				// pre-align if input is vice versa to save code (DRY)
+
+				if (game.getPlayer1().getId() == gameDTO.getPlayer2Id()
+						&& game.getPlayer2().getId() == gameDTO.getPlayer1Id()) {
+					gameDTO = alignPlayers(gameDTO);
+				}
 
 				// scores aligned?
 				if (game.getPlayer1().getId() == gameDTO.getPlayer1Id()
 						&& game.getPlayer2().getId() == gameDTO.getPlayer2Id()) {
-					for (i = 0; i < existingSets.size(); i++) {
-						existingSets.get(i).setPlayer1Score(
-								newSets.get(i).getPlayer1Score());
-						existingSets.get(i).setPlayer2Score(
-								newSets.get(i).getPlayer2Score());
 
-						updated = true;
+					ListIterator<ShowdownGameSet> setsIterator = existingSets
+							.listIterator();
+
+					while (setsIterator.hasNext()) {
+						IGameSet oldSet = setsIterator.next();
+						// DTOs get counted and index-accessed in parallel
+						GameSetDTO newSet = newSets.get(numDTOs++);
+
+						// copy scores
+						oldSet.setPlayer1Score(newSet.getPlayer1Score());
+						oldSet.setPlayer2Score(newSet.getPlayer2Score());
+
+						// write set winner
+
+						if (oldSet.getPlayer1Score() > 0
+								|| oldSet.getPlayer2Score() > 0) {
+
+							if (oldSet.getPlayer1Score() > oldSet
+									.getPlayer2Score()) {
+								oldSet.setWinner(oldSet.getGame().getPlayer1());
+
+								logger.debug("Determined game set winner: "
+										+ oldSet.getGame().getPlayer1()
+												.getName() + ", "
+										+ oldSet.getPlayer1Score() + ":"
+										+ oldSet.getPlayer2Score());
+							} else if (oldSet.getPlayer2Score() > oldSet
+									.getPlayer1Score()) {
+								oldSet.setWinner(oldSet.getGame().getPlayer2());
+
+								logger.debug("Determined game set winner: "
+										+ oldSet.getGame().getPlayer2()
+												.getName() + ", "
+										+ oldSet.getPlayer2Score() + ":"
+										+ oldSet.getPlayer1Score());
+							}
+						}
 					}
 
-				} else if (game.getPlayer1().getId() == gameDTO.getPlayer2Id()
-						&& game.getPlayer2().getId() == gameDTO.getPlayer1Id()) {
-
-					// distribute values vice versa if not in alignment
-					for (i = 0; i < existingSets.size(); i++) {
-						existingSets.get(i).setPlayer1Score(
-								newSets.get(i).getPlayer2Score());
-						existingSets.get(i).setPlayer2Score(
-								newSets.get(i).getPlayer1Score());
-
+					if (numDTOs > 0) {
 						updated = true;
+						game.setSets(existingSets);
 					}
+
 				} else {
 					logger.warn("Player ID mismatch");
 				}
 
 				if (updated) {
+					int setsPlayed = 0;
+					int setsWonByPlayer1 = 0;
+					int setsWonByPlayer2 = 0;
+
+					for (IGameSet set : game.getSets()) {
+						if (set.hasWinner()
+								&& set.getGame().getPlayer1()
+										.equals(set.getWinner())) {
+							setsPlayed++;
+							setsWonByPlayer1++;
+						} else if (set.hasWinner()
+								&& set.getGame().getPlayer2()
+										.equals(set.getWinner())) {
+							setsPlayed++;
+							setsWonByPlayer2++;
+						}
+					}
+
+					// we have a winner if
+					// - all sets have been played
+					// - one player has won more sets than the other
+
+					if (setsPlayed == game.getSets().size()) {
+						if (setsWonByPlayer1 > setsWonByPlayer2) {
+							game.setWinner(game.getPlayer1());
+
+							logger.debug("Determined game winner: "
+									+ game.getPlayer1().getName() + ", "
+									+ setsWonByPlayer1 + ":" + setsWonByPlayer2);
+						} else if (setsWonByPlayer2 > setsWonByPlayer1) {
+							game.setWinner(game.getPlayer2());
+
+							logger.debug("Determined game winner: "
+									+ game.getPlayer2().getName() + ", "
+									+ setsWonByPlayer2 + ":" + setsWonByPlayer1);
+						}
+					}
+
 					logger.debug("After: " + game);
 					// write back
-					game.setSets(existingSets);
 					gameDAO.updateGame(game);
 					success = true;
-				} else {
-					logger.warn("Unable to update game");
 				}
 			}
 		}
 
 		return success;
+	}
+
+	// switch players if necessary (player1Score becomes player2Score etc.)
+	// needed in rare case where client JSON might be mixed up
+	private GameDTO alignPlayers(GameDTO unalignedDTO) {
+		// log level info because unusual
+		logger.info("Unaligned DTO: " + unalignedDTO);
+
+		GameDTO alignedDTO = new GameDTO(unalignedDTO);
+		// vice versa
+		alignedDTO.setPlayer1Id(unalignedDTO.getPlayer2Id());
+		alignedDTO.setPlayer1Name(unalignedDTO.getPlayer2Name());
+		alignedDTO.setPlayer2Id(unalignedDTO.getPlayer1Id());
+		alignedDTO.setPlayer2Name(unalignedDTO.getPlayer1Name());
+		// align set list
+		ArrayList<GameSetDTO> alignedSets = new ArrayList<GameSetDTO>();
+		for (GameSetDTO set : unalignedDTO.getSets()) {
+			logger.info("Unaligned set: " + set);
+			// vice versa
+			// we have to set any winner to null
+			GameSetDTO alignedSet = new GameSetDTO(set.getId(),
+					set.getGameId(), 0, set.getPlayer2Score(),
+					set.getPlayer1Score(), null);
+			logger.info("Aligned set: " + alignedSet);
+
+			alignedSets.add(alignedSet);
+		}
+
+		alignedDTO.setSets(alignedSets);
+		logger.info("Aligned DTO: " + alignedDTO);
+
+		return alignedDTO;
 	}
 }
