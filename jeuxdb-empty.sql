289d8265bf6c3db9f06fc1e0733781501c56d10d
marthjod@gmail.com <marthjod@gmail.com>
2015-01-18 00:59:20 +0100

    larger ints in DB

--- jeuxdb-empty.sql
+++ jeuxdb-empty.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.12
+-- version 4.0.9
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Apr 20, 2014 at 11:37 
--- Server version: 5.6.16
--- PHP Version: 5.5.11
+-- Generation Time: Jan 17, 2015 at 08:16 PM
+-- Server version: 5.6.14
+-- PHP Version: 5.5.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -47,8 +47,8 @@ CREATE TABLE IF NOT EXISTS `Game` (
 
 CREATE TABLE IF NOT EXISTS `GameSet` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `player1Score` int(3) NOT NULL DEFAULT '0',
-  `player2Score` int(3) NOT NULL DEFAULT '0',
+  `player1Score` int(11) NOT NULL DEFAULT '0',
+  `player2Score` int(11) NOT NULL DEFAULT '0',
   `winnerId` int(11) DEFAULT NULL COMMENT 'Player who has scored more',
   `gameId` int(11) NOT NULL COMMENT 'Game this set is part of',
   `number` int(3) NOT NULL DEFAULT '1',
@@ -64,11 +64,11 @@ CREATE TABLE IF NOT EXISTS `GameSet` (
 --
 
 CREATE TABLE IF NOT EXISTS `Group_` (
-  `id` int(3) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `roundId` int(2) NOT NULL COMMENT 'Round this group belongs to',
-  `minSets` int(1) NOT NULL DEFAULT '1' COMMENT 'Minimum number of sets to be played in each of the group''s games',
-  `maxSets` int(2) NOT NULL DEFAULT '1' COMMENT 'Maximum number of sets to be played in each of the group''s games',
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` text COLLATE utf8_unicode_ci NOT NULL,
+  `roundId` int(11) NOT NULL COMMENT 'Round this group belongs to',
+  `minSets` int(3) NOT NULL DEFAULT '1' COMMENT 'Minimum number of sets to be played in each of the group''s games',
+  `maxSets` int(4) NOT NULL DEFAULT '1' COMMENT 'Maximum number of sets to be played in each of the group''s games',
   `active` tinyint(1) NOT NULL DEFAULT '0' COMMENT 'Is this group''s games currently being played?',
   `completed` tinyint(1) NOT NULL DEFAULT '0' COMMENT 'Have all this group''s games been played?',
   PRIMARY KEY (`id`)
@@ -81,13 +81,13 @@ CREATE TABLE IF NOT EXISTS `Group_` (
 --
 
 CREATE TABLE IF NOT EXISTS `Player` (
-  `id` int(3) NOT NULL AUTO_INCREMENT,
-  `groupId` int(3) NOT NULL COMMENT 'Group this player currently belongs to',
-  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `points` int(3) NOT NULL DEFAULT '0' COMMENT 'Total points (0-n) player has been awarded for won sets. May be 1 per won game if only 1 set is played per game',
-  `scoreRatio` int(4) DEFAULT NULL COMMENT 'Difference (-n...0...n) between player''s own and opponent''s scores (in games which this player participated in)',
-  `rank` int(2) NOT NULL DEFAULT '0' COMMENT 'Rank in group. Updated after all this group''s games have been completed',
-  `wonGames` int(3) NOT NULL DEFAULT '0',
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `groupId` int(11) NOT NULL COMMENT 'Group this player currently belongs to',
+  `name` text COLLATE utf8_unicode_ci NOT NULL,
+  `points` int(11) NOT NULL DEFAULT '0' COMMENT 'Total points (0-n) player has been awarded for won sets. May be 1 per won game if only 1 set is played per game',
+  `scoreRatio` int(11) DEFAULT NULL COMMENT 'Difference (-n...0...n) between player''s own and opponent''s scores (in games which this player participated in)',
+  `rank` int(11) NOT NULL DEFAULT '0' COMMENT 'Rank in group. Updated after all this group''s games have been completed',
+  `wonGames` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
   KEY `fk_Player_Group1_idx` (`groupId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
@@ -102,9 +102,9 @@ CREATE TABLE IF NOT EXISTS `RoundSwitchRule` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `srcGroupId` int(11) NOT NULL COMMENT 'Group where players originate from.',
   `destGroupId` int(11) NOT NULL COMMENT 'Group players should be in in the next round.',
-  `startWithRank` int(3) NOT NULL COMMENT 'Player with this rank in source group is the first to be moved from source group to destination group.',
-  `additionalPlayers` int(3) NOT NULL COMMENT 'How many additional players should be moved from source to destination group?',
-  `previousRoundId` int(3) NOT NULL COMMENT '(For safety. Remove if unused.) ',
+  `startWithRank` int(11) NOT NULL COMMENT 'Player with this rank in source group is the first to be moved from source group to destination group.',
+  `additionalPlayers` int(11) NOT NULL COMMENT 'How many additional players should be moved from source to destination group?',
+  `previousRoundId` int(11) NOT NULL COMMENT '(For safety. Remove if unused.) ',
   PRIMARY KEY (`id`),
   KEY `fk_rule_src_group` (`srcGroupId`),
   KEY `fk_rule_dest_group` (`destGroupId`)
