86b0a59706aade55614e6e1be33f885df557de6b
marthjod@gmail.com <marthjod@gmail.com>
2015-03-29 23:04:25 +0200

    check for missing SERVER_SECRET on auth token generation

--- jeux-web/src/main/java/de/fhb/jeux/controller/AdminLoginServlet.java
+++ jeux-web/src/main/java/de/fhb/jeux/controller/AdminLoginServlet.java
@@ -1,54 +1,54 @@
 package de.fhb.jeux.controller;
 
+import de.fhb.jeux.util.AuthUtils;
 import java.io.IOException;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import org.jboss.logging.Logger;
-
-import de.fhb.jeux.util.AuthUtils;
 
 public class AdminLoginServlet extends HttpServlet {
 
-	private static final long serialVersionUID = 1L;
-	protected static Logger logger = Logger.getLogger(AdminLoginServlet.class);
+    private static final long serialVersionUID = 1L;
+    protected static Logger logger = Logger.getLogger(AdminLoginServlet.class);
 
-	public AdminLoginServlet() {
-		super();
-	}
+    public AdminLoginServlet() {
+        super();
+    }
 
-	@Override
-	protected void doGet(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
+    @Override
+    protected void doGet(HttpServletRequest request,
+            HttpServletResponse response) throws ServletException, IOException {
 
-		String nextView = response.encodeRedirectURL(request.getContextPath());
+        String nextView = response.encodeRedirectURL(request.getContextPath());
 
-		// TODO add some real #security
+        // TODO add some real #security
+        String serverSecret = getServletConfig().getInitParameter(
+                "SERVER_SECRET");
 
-		String serverSecret = getServletConfig().getInitParameter(
-				"SERVER_SECRET");
+        if (serverSecret == null || serverSecret.length() == 0) {
+            logger.error("Mandatory server secret not set, unable to generate auth tokens");
+        } else {
+            // TODO different user names #security
+            // TODO different cookies for different users/access times #security
+            String authToken = AuthUtils.generateAuthToken("admin", serverSecret);
 
-		// TODO different user names #security
-		// TODO different cookies for different users/access times #security
-		String authToken = AuthUtils.generateAuthToken("admin", serverSecret);
+            if (authToken.length() > 0) {
+                // TODO no hardcoded naming #app-design
+                Cookie authCookie = new Cookie("admin-auth", authToken);
+                logger.debug("Returning cookie " + authCookie.getName() + "="
+                        + authCookie.getValue());
+                response.addCookie(authCookie);
+                // TODO no hardcoded naming #app-design
+                nextView = response.encodeRedirectURL(request.getContextPath()
+                        + "/admin.html");
+            } else {
+                logger.warn("Could not generate authentification token.");
+            }
+        }
 
-		if (authToken.length() > 0) {
-			// TODO no hardcoded naming #app-design
-			Cookie authCookie = new Cookie("admin-auth", authToken);
-			logger.debug("Returning cookie " + authCookie.getName() + "="
-					+ authCookie.getValue());
-			response.addCookie(authCookie);
-			// TODO no hardcoded naming #app-design
-			nextView = response.encodeRedirectURL(request.getContextPath()
-					+ "/admin.html");
-		} else {
-			logger.warn("Could not generate authentification token.");
-		}
-
-		response.sendRedirect(nextView);
-	}
+        response.sendRedirect(nextView);
+    }
 }
