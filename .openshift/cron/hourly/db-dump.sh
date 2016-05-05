001b59209837258e7f0acaa91d7d78c50c54b5c0
marthjod@gmail.com <marthjod@gmail.com>
2016-05-05 19:42:01 +0200

    changes to DB dump Cron script

--- .openshift/cron/hourly/db-dump.sh
+++ .openshift/cron/hourly/db-dump.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-#mysqldump -h $OPENSHIFT_MYSQL_DB_HOST \
-#	-u $OPENSHIFT_MYSQL_DB_USERNAME \
-# 	-p"$OPENSHIFT_MYSQL_DB_PASSWORD" \
-#	-P $OPENSHIFT_MYSQL_DB_PORT \
-#	jeux \
-#	> $OPENSHIFT_DATA_DIR/jeux-$(date +%Y%m%d-%H%M%S).sql
+db_name="jeux"
+enabled=0
+if [ $enabled -ne 1 ]; then
+    exit
+fi
+
+dumpfile=$OPENSHIFT_DATA_DIR/$db_name-$(date +%FT%T).sql
+mysqldump -h $OPENSHIFT_MYSQL_DB_HOST \
+	-u $OPENSHIFT_MYSQL_DB_USERNAME \
+ 	-p"$OPENSHIFT_MYSQL_DB_PASSWORD" \
+	-P $OPENSHIFT_MYSQL_DB_PORT \
+	$db_name \
+	> $dumpfile
+echo "Dumped to $dumpfile."
 
